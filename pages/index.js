b41d1efcd3fac7351dcc5e32c967de418bac57ec
pgnaguit <pgmnaguit.student@ua.edu.ph>
2026-08-18 11:39:02 +0800

    Update title to use siteTitle variable

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <Layout home>
       <Head>
-        <title>{Patrick}</title>
+        <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hello! I'm Patrick Naguit,  a student from University of the Assumption.</p>
