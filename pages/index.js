15d13b57f35b04e7dc584b8ed67f7eadb4bc082a
pgnaguit <pgmnaguit.student@ua.edu.ph>
2026-08-18 11:49:39 +0800

    Revise personal introduction in index.js
    Updated personal introduction with more details about interests and future aspirations.

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello! I'm Patrick Naguit,  a student from University of the Assumption.</p>
+        <p>Hi! I’m Patrick Gabriel M. Naguit, a BSIT student who enjoys playing games, listening to OPM and pop music, watching movies, and spending time with family and friends. My favorite foods include salmon, sisig, and chicken barbecue. Five years from now, I see myself as a successful IT professional with a stable career, continuously improving my skills and achieving my goals.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
